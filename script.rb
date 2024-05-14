5d029447db630189855722496896e5ef2d9978af
Aleem Isiaka <aleemisiaka@gmail.com>
2024-05-14 14:26:24 +0100

    fill in form

--- script.rb
+++ script.rb
@@ -22,7 +22,7 @@ class Spider < Tanakai::Base
 
         found_avatar = browser.has_css?(".s-avatar--image")
         browser.click_on "Accept all cookies"
-        browser.find("//*[@id='question-mini-list']//a").first.click
+        browser.find("//*[@id='question-mini-list']//a", match: :first).click
         puts found_avatar ? "Successfully logged in to Stackoverflow today!" : "Unable to Login today"
     end
 end
